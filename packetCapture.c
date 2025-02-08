4afe3c9d722175a34b8f6a98ef84747dc8d6dd78
nengkya <nengkya@yahoo.com>
2025-02-08 09:33:28 +0700

    #define PCAP_ERRBUF_SIZE 256*

--- packetCapture.c
+++ packetCapture.c
@@ -1,35 +1,38 @@
+#include "pcap.h"
+
+
 int main() {
 
     /*
     typedef pcap_if pcap_if_t
 
     pcap_if * next
-	if not NULL, a pointer to the next element in the list; NULL for the last element of the list.
+		if not NULL, a pointer to the next element in the list; NULL for the last element of the list.
     char * name
-	a pointer to a string giving a name for the device to pass to pcap_open_live()
+		a pointer to a string giving a name for the device to pass to pcap_open_live()
     char * description
-	if not NULL, a pointer to a string giving a human-readable description of the device
+		if not NULL, a pointer to a string giving a human-readable description of the device
     pcap_addr * addresses
-	a pointer to the first element of a list of addresses for the interface
+		a pointer to the first element of a list of addresses for the interface
     u_int flags
-	PCAP_IF_ interface flags. Currently the only possible flag is PCAP_IF_LOOPBACK,
-	that is set if the interface is a loopback interface.
+		PCAP_IF_ interface flags. Currently the only possible flag is PCAP_IF_LOOPBACK,
+		that is set if the interface is a loopback interface.
 
     struct pcap_addr * next
-	if not NULL, a pointer to the next element in the list; NULL for the last element of the list
+		if not NULL, a pointer to the next element in the list; NULL for the last element of the list
     struct sockaddr * addr
-	a pointer to a struct sockaddr containing an address
+		a pointer to a struct sockaddr containing an address
     struct sockaddr * netmask
-	if not NULL, a pointer to a struct sockaddr
-	that contains the netmask corresponding to the address pointed to by addr.
+		if not NULL, a pointer to a struct sockaddr
+		that contains the netmask corresponding to the address pointed to by addr.
     struct sockaddr * broadaddr
-	if not NULL, a pointer to a struct sockaddr
-	that contains the broadcast address corresponding to the address pointed to by addr;
-	may be null if the interface doesn't support broadcasts
+		if not NULL, a pointer to a struct sockaddr
+		that contains the broadcast address corresponding to the address pointed to by addr;
+		may be null if the interface doesn't support broadcasts
     struct sockaddr * dstaddr
-	if not NULL, a pointer to a struct sockaddr
-	that contains the destination address corresponding to the address pointed to by addr;
-	may be null if the interface isn't a point- to-point interface
+		if not NULL, a pointer to a struct sockaddr
+		that contains the destination address corresponding to the address pointed to by addr;
+		may be null if the interface isn't a point- to-point interface
 
     struct sockaddr {
 	ushort  sa_family;   //sa = socket address
@@ -61,5 +64,13 @@ int main() {
     
     */
     pcap_if_t * allDevices;
+
+	/*#define PCAP_ERRBUF_SIZE 256*/
+	char errorBuffer[PCAP_ERRBUF_SIZE];
+
+	/*int pcap_findalldevs(pcap_if_t ** alldevsp, char * errbuf);*/
+	pcap_findalldevs(&allDevices, errorBuffer);
+
+	
 
 }
