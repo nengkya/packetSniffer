0db0f4a54a75b41f99a9c7b876e729853e7895b8
Hasna Dzakiyya Makin <nengkya@yahoo.com>
2025-02-10 20:14:04 +0700

    char * device = pcap_lookupdev(errorBuffer);

---
+++ tcpdump.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+int main() {
+    char * errorBuffer = (char *)malloc(PCAP_ERR_:qsizeof(char *));
+    char * device = pcap_lookupdev(errorBuffer);
+    fprintf(stderr, "%s",errorBuffer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
