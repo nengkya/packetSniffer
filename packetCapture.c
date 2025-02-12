5b59d5c9e0a83180fb46654155a4f5532816158b
nengkya <nengkya@yahoo.com>
2025-02-12 12:16:35 +0700

    merge

--- packetCapture.c
+++ packetCapture.c
@@ -6,39 +6,39 @@ int main() {
     /*
     typedef pcap_if pcap_if_t {
 
-		pcap_if * next
-		    if not NULL, a pointer to the next element in the list;
-		    NULL for the last element of the list.
-		char * name
-		    a pointer to a string giving a name for the device to pass to pcap_open_live()
-		char * description
-		    if not NULL, a pointer to a string giving a human-readable description of the device
-		pcap_addr * addresses
-		    a pointer to the first element of a list of addresses for the interface
-		u_int flags
-		    PCAP_IF_ interface flags. Currently the only possible flag is PCAP_IF_LOOPBACK,
-		    that is set if the interface is a loopback interface.
-		struct pcap_addr * next
-		    if not NULL, a pointer to the next element in the list;
-		    NULL for the last element of the list
-		struct sockaddr * addr
-		    a pointer to a struct sockaddr containing an address
-		struct sockaddr * netmask
-		    if not NULL, a pointer to a struct sockaddr
-		    that contains the netmask corresponding to the address pointed to by addr.
-		struct sockaddr * broadaddr
-		    if not NULL, a pointer to a struct sockaddr
-		    that contains the broadcast address corresponding to the address pointed to by addr;
-		    may be null if the interface doesn't support broadcasts
-		struct sockaddr * dstaddr
-		    if not NULL, a pointer to a struct sockaddr
-		    that contains the destination address corresponding to the address pointed to by addr;
-		    may be null if the interface isn't a point- to-point interface
+	pcap_if * next
+	    if not NULL, a pointer to the next element in the list;
+	    NULL for the last element of the list.
+	char * name
+	    a pointer to a string giving a name for the device to pass to pcap_open_live()
+	char * description
+	    if not NULL, a pointer to a string giving a human-readable description of the device
+	pcap_addr * addresses
+	    a pointer to the first element of a list of addresses for the interface
+	u_int flags
+	    PCAP_IF_ interface flags. Currently the only possible flag is PCAP_IF_LOOPBACK,
+	    that is set if the interface is a loopback interface.
+	struct pcap_addr * next
+	    if not NULL, a pointer to the next element in the list;
+	    NULL for the last element of the list
+	struct sockaddr * addr
+	    a pointer to a struct sockaddr containing an address
+	struct sockaddr * netmask
+	    if not NULL, a pointer to a struct sockaddr
+	    that contains the netmask corresponding to the address pointed to by addr.
+	struct sockaddr * broadaddr
+	    if not NULL, a pointer to a struct sockaddr
+	    that contains the broadcast address corresponding to the address pointed to by addr;
+	    may be null if the interface doesn't support broadcasts
+	struct sockaddr * dstaddr
+	    if not NULL, a pointer to a struct sockaddr
+	    that contains the destination address corresponding to the address pointed to by addr;
+	    may be null if the interface isn't a point- to-point interface
     };
 
     struct sockaddr {
-		ushort  sa_family;   //sa = socket address
-		char    sa_data[14];
+	ushort  sa_family;   //sa = socket address
+	char    sa_data[14];
     };
 
     sa_family is socket address spesific family structure.
@@ -78,76 +78,77 @@ int main() {
     if (pcap_findalldevs(&allDevices, errorBuffer) == -1)
        	fprintf(stderr, "%s\n",errorBuffer);
 
-	    for (device = allDevices; device != NULL; device = device->next) {
-	        printf("%d. %s ", ++i, device->name);
-		if (device->description)
-		    printf("%s", device->description);
-		printf("\n");
+    for (device = allDevices; device != NULL; device = device->next) {
+	printf("%d. %s ", ++i, device->name);
+	if (device->description)
+	    printf("%s", device->description);
+	printf("\n");
     }
 
     /*opening the device for sniffing
     typedef struct pcap pcap_t
     struct pcap {
-		int fd;
-		int snapshot;
-		int linktype;
-		int tzoff;      		//timezone offset
-		int offset;     		//offset for proper alignment
-		struct pcap_sf sf;
-		struct pcap_md md;
-		int bufsize;			//read buffer
-		u_char * buffer;
-		u_char * bp;			//buff pointer
-		int cc;				//current capture
-		u_char * pkt;			//place holder for packet pcap_next()
-		//placeholder for filter code if bpf (barkley packet filter) not in kernel
-		struct bpf_program {
-		struct bpf_insn * bf_insns; 	//pointer to an array of bp finstructions
-		u_int  bf_len;              	//number of instructions in the array
+	int fd;
+	int snapshot;
+	int linktype;
+	int tzoff;      		//timezone offset
+	int offset;     		//offset for proper alignment
+	struct pcap_sf sf;
+	struct pcap_md md;
+	int bufsize;			//read buffer
+	u_char * buffer;
+	u_char * bp;			//buff pointer
+	int cc;				//current capture
+	u_char * pkt;			//place holder for packet pcap_next()
+	//placeholder for filter code if bpf (barkley packet filter) not in kernel
+	struct bpf_program {
+	struct bpf_insn * bf_insns; 	//pointer to an array of bp finstructions
+	u_int  bf_len;              	//number of instructions in the array
     };
 
-	struct pcap_sf {
-		size_t hdrsize;
-		swapped_type_t lengths_swapped;
-		tstamp_scale_type_t scale_type;
-	};
+    struct pcap_sf {
+	size_t hdrsize;
+	swapped_type_t lengths_swapped;
+	tstamp_scale_type_t scale_type;
+    };
 
-	typedef enum {
-		NOT_SWAPPED,
-		SWAPPED,
-		MAYBE_SWAPPED
-	} swapped_type_t;
-	
-	typedef enum {
-		TSTAMP_SCALE_SECONDS
-		TSTAMP_SCALE_MILLISECONDS
-		TSTAMP_SCALE_MICROSECONDS
-		TSTAMP_SCALE_NANOSECONDS
-		TSTAMP_SCALE_UNKNOWN (for an undefined or error case)
-	} tstamp_scale_type_t
+    typedef enum {
+	NOT_SWAPPED,
+	SWAPPED,
+	MAYBE_SWAPPED
+    } swapped_type_t;
+    
+    typedef enum {
+	TSTAMP_SCALE_SECONDS
+	TSTAMP_SCALE_MILLISECONDS
+	TSTAMP_SCALE_MICROSECONDS
+	TSTAMP_SCALE_NANOSECONDS
+	TSTAMP_SCALE_UNKNOWN (for an undefined or error case)
+    } tstamp_scale_type_t
 
-	timestamp types are integer constants rather than typedef tstamp_scale_type_t.
- 	these constants specify the source and characteristics of the timestamp applied to captured packets.
-  	the available timestamp types include :
-	PCAP_TSTAMP_HOST: Timestamp provided by the host machine.
-	PCAP_TSTAMP_HOST_LOWPREC: Low-precision timestamp from the host.
-	PCAP_TSTAMP_HOST_HIPREC: High-precision timestamp from the host.
-	PCAP_TSTAMP_HOST_HIPREC_UNSYNCED: High-precision, unsynchronized timestamp from the host.
-	PCAP_TSTAMP_ADAPTER: Timestamp provided by the network adapter.
-	PCAP_TSTAMP_ADAPTER_UNSYNCED: Timestamp from the network adapter, not synchronized with the system clock.
+    timestamp types are integer constants rather than typedef tstamp_scale_type_t
+    these constants specify the source and characteristics of the timestamp applied to captured packets
+    the available timestamp types include :
+    PCAP_TSTAMP_HOST: Timestamp provided by the host machine
+    PCAP_TSTAMP_HOST_LOWPREC: Low-precision timestamp from the host
+    PCAP_TSTAMP_HOST_HIPREC: High-precision timestamp from the host
+    PCAP_TSTAMP_HOST_HIPREC_UNSYNCED: High-precision, unsynchronized timestamp from the host
+    PCAP_TSTAMP_ADAPTER: Timestamp provided by the network adapter
+    PCAP_TSTAMP_ADAPTER_UNSYNCED: timestamp from the network adapter,
+				  not synchronized with the system clock
  
     struct bpf_insn {
-		u_short code;  //operation code (opcode)
-		u_char  jt;    //jump offset if true
-		u_char  jf;    //jump offset if false
-		u_int   k;     //generic field for constants, offsets, or addresses
+	u_short code;  //operation code (opcode)
+	u_char  jt;    //jump offset if true
+	u_char  jf;    //jump offset if false
+	u_int   k;     //generic field for constants, offsets, or addresses
     };
 
     struct bpf_insn insn = {
-		.code = BPF_LD | BPF_H | BPF_ABS,//load 16-bit halfword at absolute offset
-		.jt   =  0,						 //no jump
-		.jf	  =  0,						 //no jump
-		.k	  = 12		   //offset where ether type field is located in ethernet frame
+	.code = BPF_LD | BPF_H | BPF_ABS,//load 16-bit halfword at absolute offset
+	.jt   =  0,						 //no jump
+	.jf	  =  0,						 //no jump
+	.k	  = 12		   //offset where ether type field is located in ethernet frame
     };
 
     bpf opcode	meaning
